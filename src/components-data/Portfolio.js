d5699e30ce10c6332c6f3c7f456694955aa5f038
MeteBely <metehanmuradoglu17@gmail.com>
2024-06-02 02:31:16 +0300

    portfolio ex. datas

---
+++ src/components-data/Portfolio.js
@@ -0,0 +1,47 @@
+export const PortfolioProjects = [
+  {
+    title: "Web Application",
+    subTxt: "Lorem ipsum, dolor sit",
+    img: "https://bootstrapmade.com/demo/templates/Arsha/assets/img/masonry-portfolio/masonry-portfolio-1.jpg",
+  },
+  {
+    title: "Highly Customabimlze",
+    subTxt: "Lorem ipsum, dolor sit",
+    img: "https://bootstrapmade.com/demo/templates/Arsha/assets/img/masonry-portfolio/masonry-portfolio-6.jpg",
+  },
+  {
+    title: "Web Application",
+    subTxt: "Lorem ipsum, dolor sit",
+    img: "https://bootstrapmade.com/demo/templates/Arsha/assets/img/masonry-portfolio/masonry-portfolio-2.jpg",
+  },
+  {
+    title: "Optimised for Speed",
+    subTxt: "Lorem ipsum, dolor sit",
+    img: "https://bootstrapmade.com/demo/templates/Arsha/assets/img/masonry-portfolio/masonry-portfolio-5.jpg",
+  },
+  {
+    title: "Service & Plugins",
+    subTxt: "Lorem ipsum, dolor sit",
+    img: "https://bootstrapmade.com/demo/templates/Arsha/assets/img/masonry-portfolio/masonry-portfolio-4.jpg",
+  },
+  {
+    title: "Dedicated Support",
+    subTxt: "Lorem ipsum, dolor sit",
+    img: "https://bootstrapmade.com/demo/templates/Arsha/assets/img/masonry-portfolio/masonry-portfolio-3.jpg",
+  },
+  {
+    title: "Dedicated Support",
+    subTxt: "Lorem ipsum, dolor sit",
+    img: "https://bootstrapmade.com/demo/templates/Arsha/assets/img/masonry-portfolio/masonry-portfolio-7.jpg",
+  },
+  {
+    title: "Dedicated Support",
+    subTxt: "Lorem ipsum, dolor sit",
+    img: "https://bootstrapmade.com/demo/templates/Arsha/assets/img/masonry-portfolio/masonry-portfolio-8.jpg",
+  },
+  {
+    title: "Dedicated Support",
+    subTxt: "Lorem ipsum, dolor sit",
+    img: "https://bootstrapmade.com/demo/templates/Arsha/assets/img/masonry-portfolio/masonry-portfolio-9.jpg",
+  },
+];
