15e5bafb8cafb4b84bbf08a1d9fd587c5b12bcc6
MeteBely <metehanmuradoglu17@gmail.com>
2024-06-19 22:47:38 +0300

    header company icon entegresi tamamlandı, stil düzeltmeleri yapıldı

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,35 @@
 import { useEffect, useState } from "react";
+import jforceIcon from "../assets/jforceIcon.png";
+import baby from "../assets/baby.png";
 
 const Header = () => {
   const [small, setSmall] = useState(false);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
-      window.addEventListener("scroll", () =>
-        setSmall(window.pageYOffset > 200)
-      );
+      window.addEventListener("scroll", () => {
+        setSmall(window.pageYOffset > 200);
+      });
     }
   }, []);
 
   return (
     <header>
       <div
-        className={`flex flex-row justify-around items-center h-20 fixed w-full text-[#DEE2EA] fontMontserrat font-semibold transition-all duration-500 z-50 ${
-          small && "bg-[#293C5D] opacity-95"
+        className={`flex flex-row justify-evenly items-center h-20 fixed w-full text-[#000002] fontMontserrat font-semibold transition-all duration-500 z-50 ${
+          small && "bg-[#303030] opacity-95 text-[#fff]"
         }`}
       >
-        <div>
-          <a href="">ETEM COMPANY with ICON</a>
+        <div className="w-[500px]">
+          <a href="" className="pointer-events-none">
+            {small ? (
+              <img className="w-[500px]" src={baby} alt="" />
+            ) : (
+              <div className="w-[500px] ml-32">
+                <img src={jforceIcon} />
+              </div>
+            )}
+          </a>
         </div>
         <div>
           <ul className="flex flex-row gap-8 items-center justify-center text-lg roboto-medium tracking-wider">
