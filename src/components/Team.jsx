29369d8a55db1d659a69ad7a15a125d6d3c2d500
MeteBely <metehanmuradoglu17@gmail.com>
2024-06-22 18:50:56 +0300

    team component responsive yapıldı

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -4,17 +4,17 @@ import TeamMemberItem from "./TeamMemberItem";
 const Team = () => {
   return (
     <section>
-      <div className="bg-[#F5F6F8] py-40">
-        <div className="mb-20">
-          <h2 className="text-[52px] font-extrabold fontMontserrat pb-4 mb-4 text-[#000] border-b border-gray-400 mx-auto text-center w-[675px]">
+      <div className="bg-[#F5F6F8] py-20 md:py-40">
+        <div className="mb-10 md:mb-20 px-4">
+          <h2 className="text-[32px] md:text-[52px] font-extrabold fontMontserrat pb-4 mb-4 text-[#000] border-b border-gray-400 mx-auto text-center w-full max-w-[675px]">
             MANAGEMENT TEAM
           </h2>
-          <p className="mx-auto fontRoboto text-center w-[800px]">
+          <p className="mx-auto fontRoboto text-center w-full max-w-[800px]">
             Necessitatibus eius consequatur ex aliquid fuga eum quidem sint
             consectetur velit
           </p>
         </div>
-        <div className="flex flex-row justify-center items-center flex-wrap gap-8 w-[1400px] m-auto">
+        <div className="flex flex-col md:flex-row justify-center items-center flex-wrap gap-8 w-full max-w-[1400px] m-auto px-4">
           {TeamMembers.map((member, index) => (
             <TeamMemberItem member={member} key={index} index={index} />
           ))}
--- src/components/TeamMemberItem.jsx
+++ src/components/TeamMemberItem.jsx
@@ -13,39 +13,45 @@ const TeamMemberItem = ({ member, index }) => {
 
   return (
     <div
-      className="w-[600px] h-[220px] flex flex-row items-start pt-6 justify-center gap-6 shadow-md rounded-md bg-white px-1 hover:-translate-y-2 duration-500"
       data-aos="fade-up"
       data-aos-delay={index === 1 || index === 3 ? "200" : "0"}
     >
-      <div>
-        <img className="w-40 rounded-full" src={member.img} alt="" />
-      </div>
-      <div>
-        <h4 className="text-[#000] text-2xl font-semibold fontMontserrat">
-          {member.name}
-        </h4>
-        <p className="mb-4  w-[160px] underline underline-offset-[5px] fontRoboto decoration-slate-400">
-          {member.position}
-        </p>
-        <p className="text-[15px] mb-6 fontRoboto ">{member.subTxt}</p>
-        <div className="flex flex-row items-center justify-start gap-6">
-          <a
-            className="cursor-pointer"
-            onMouseEnter={() => setIconUp("x")}
-            onMouseLeave={() => setIconUp("")}
-          >
-            <FaXTwitter size={24} color={iconUp === "x" ? "#000" : "#6F6F6F"} />
-          </a>
-          <a
-            className="cursor-pointer"
-            onMouseEnter={() => setIconUp("linkedin")}
-            onMouseLeave={() => setIconUp("")}
-          >
-            <FaLinkedin
-              size={24}
-              color={iconUp === "linkedin" ? "#0B65C2" : "#6F6F6F"}
-            />
-          </a>
+      <div className="w-full max-w-[600px] h-auto md:h-[220px] flex flex-col md:flex-row items-center md:items-start pt-6 justify-center md:gap-6 shadow-md rounded-md bg-white px-1 pb-3 md:pb-0 hover:-translate-y-2 duration-500">
+        <div className="mb-2 md:mb-0">
+          <img className="w-28 md:w-40 rounded-full" src={member.img} alt="" />
+        </div>
+        <div className="text-center md:text-left">
+          <h4 className="text-[#000] text-xl md:text-2xl font-semibold fontMontserrat">
+            {member.name}
+          </h4>
+          <p className="mb-2 md:mb-4 w-full md:w-[160px] underline underline-offset-[5px] fontRoboto decoration-slate-400">
+            {member.position}
+          </p>
+          <p className="text-[13px] md:text-[15px] mb-2 md:mb-6 fontRoboto">
+            {member.subTxt}
+          </p>
+          <div className="flex flex-row items-center justify-center md:justify-start gap-6">
+            <a
+              className="cursor-pointer"
+              onMouseEnter={() => setIconUp("x")}
+              onMouseLeave={() => setIconUp("")}
+            >
+              <FaXTwitter
+                size={24}
+                color={iconUp === "x" ? "#000" : "#6F6F6F"}
+              />
+            </a>
+            <a
+              className="cursor-pointer"
+              onMouseEnter={() => setIconUp("linkedin")}
+              onMouseLeave={() => setIconUp("")}
+            >
+              <FaLinkedin
+                size={24}
+                color={iconUp === "linkedin" ? "#0B65C2" : "#6F6F6F"}
+              />
+            </a>
+          </div>
         </div>
       </div>
     </div>
