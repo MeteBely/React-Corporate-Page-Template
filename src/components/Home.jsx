3b228e665f596380185e136745b81a54e8815bbc
MeteBely <metehanmuradoglu17@gmail.com>
2024-06-27 15:16:11 +0300

    particles paketi entegre edildi ve kullanıldı

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,7 @@ import { BsFacebook } from "react-icons/bs";
 import { useEffect, useState } from "react";
 import Aos from "aos";
 import "aos/dist/aos.css";
+import ParticlesComponent from "./ParticlesEffect";
 
 const Home = () => {
   const [hoverBtn, setHoverBtn] = useState(false);
@@ -19,6 +20,7 @@ const Home = () => {
         style={{ backgroundImage: `url(${homePageTasarim})` }}
         className="h-[80vh] sm:h-screen w-full bg-no-repeat bg-cover max-lg:bg-top sm:bg-fixed mb-40 relative"
       >
+        <ParticlesComponent id="tsparticles-custom" />
         <div className="absolute left-[3%] sm:left-[5%] md:left-[10%] top-[15%] min-[855px]:top-[20%] lg:top-[25%] w-[90%] sm:w-[80%] md:w-[60%] lg:w-[50%]">
           <h1
             className="fontMontserrat text-[32px] lg:w-[600px] 2xl:w-[700px] sm:text-[40px] min-[855px]:text-[48px] lg:text-[52px] font-semibold leading-[40px] sm:leading-[50px] min-[855px]:leading-[55px] lg:leading-[60px] tracking-wide mb-[10px]"
@@ -27,7 +29,7 @@ const Home = () => {
             Delivering Superior Services Cutting-Edge IT Solutions
           </h1>
           <p
-            className="fontRoboto font-semibold text-[#9A979C] 2xl:w-[800px] text-[14px] sm:text-[16px] min-[855px]:text-[17px] mb-8 min-[855px]:mb-12"
+            className="fontRoboto font-semibold text-[#696969] 2xl:w-[800px] text-[14px] sm:text-[16px] min-[855px]:text-[17px] mb-8 min-[855px]:mb-12"
             data-aos="fade-up"
             data-aos-delay="400"
           >
